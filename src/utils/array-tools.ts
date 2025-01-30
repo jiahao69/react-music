c452c15aa284c20aa331b5fbde51bc04f99dbc42
从心 <congxin@congxindeMacBook-Air.local>
2025-01-30 13:16:17 +0800

    feat::添加数组工具方式文件

--- src/utils/array-tools.ts
+++ src/utils/array-tools.ts
@@ -1,4 +1,16 @@
-// 数组相关的工具函数
+/**
+ * 洗牌算法函数 可以打乱一个数组的顺序
+ */
+export const shuffle = <T>(arr: T[]) => {
+  let n = arr.length,
+    random
+
+  while (0 !== n) {
+    random = (Math.random() * n--) | 0
+    ;[arr[n], arr[random]] = [arr[random], arr[n]]
+  }
+  return arr
+}
 
 /**
  * 移动数组指定元素到首位
